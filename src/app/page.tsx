8236b4aca03c5155931d30bc346329695d1b8e5e
Musa Bukhari <applemoosa123@gmail.com>
2024-02-21 15:33:51 +0500

    text-link class added for home page links.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 export default function Home() {
   return (
     <main className="w-full h-full bg-gray-200 flex flex-col justify-center items-center">
-      <h2 className="m-[6px] p-[8px] bg-gray-400/90 rounded-[10px]">
-        <a href="https://github.com/MusaBukhari237/book-store-api">
+      <h2 className="m-[6px] p-[8px] bg-gray-400/50 rounded-[10px] ">
+        <a
+          className="text-link"
+          href="https://github.com/MusaBukhari237/book-store-api"
+        >
           Book Store Api
-        </a>
+        </a>{" "}
         Developed with <span className="text-red-700">❤</span> by{" "}
-        <a href="https://github.com/MusaBukhari237">Musa Bukhari</a>
+        <a className="text-link" href="https://github.com/MusaBukhari237">
+          Musa Bukhari
+        </a>
       </h2>
     </main>
   );
